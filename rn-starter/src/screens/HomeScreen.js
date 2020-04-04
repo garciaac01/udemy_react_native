980718b37865ed547cb6d50c1c020679195a1460
garciaac01 <garciaac01@gmail.com>
2020-04-04 14:32:10 -0400

    Adding a button and doing a console.log on click.

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <>
-      <Text style={styles.text}>Mista Parker!!</Text>
-    </>
+    <View>
+      <Text style={styles.text}>Hi there!</Text>
+      <Button
+        title='Go to Components Demo'
+        onPress={() => console.log('Button pressed')}
+      />
+    </View>
   );
 };
 
