96b42347724a696758aa132ad2b5c99ee7e75bec
garciaac01 <garciaac01@gmail.com>
2020-04-03 20:31:07 -0400

    programming challenge

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    const greeting = <Text>Hello to you!</Text>;
+    const name = 'Andy';
 
     return (
         <View>
-            <Text style={styles.textStyle}>This is the components screen</Text>
-            {greeting}
+            <Text style={styles.headerStyle}>Getting started with react native!</Text>
+            <Text style={styles.subheaderStyle}>My name is {name}</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    textStyle: {
-        fontSize: 30
+    headerStyle: {
+        fontSize: 45
+    },
+    subheaderStyle: {
+        fontSize: 20
     }
 });
 
