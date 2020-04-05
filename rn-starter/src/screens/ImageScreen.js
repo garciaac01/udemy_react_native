849de38894ee2063dbd5e440cd51b7ec870ddde4
garciaac01 <garciaac01@gmail.com>
2020-04-05 16:33:48 -0400

    Renaming imgScore to score. Destructuring props in imageDetail.

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} imgScore={9} />
-            <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} imgScore={7} />
-            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} imgScore={10} />
+            <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} score={9} />
+            <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} score={7} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} score={10} />
         </View>
     );
 }
