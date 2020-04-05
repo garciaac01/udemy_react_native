5d36762cd6a36ad4d38677977b086d87de18359e
garciaac01 <garciaac01@gmail.com>
2020-04-05 16:32:22 -0400

    Coding challenge - passing a number prop from parent to child.

--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} />
-            <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} />
-            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} />
+            <ImageDetail title='Forest' imageSource={require('../../assets/forest.jpg')} imgScore={9} />
+            <ImageDetail title='Beach' imageSource={require('../../assets/beach.jpg')} imgScore={7} />
+            <ImageDetail title='Mountain' imageSource={require('../../assets/mountain.jpg')} imgScore={10} />
         </View>
     );
 }
